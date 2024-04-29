cebd441ecbb49c4b4993f47f6452ad70975ed8f4
mukulyadav5542 <mukulyadavjal@gmail.com>
2024-04-29 13:24:01 +0530

    Delete Movie Error Check

--- src/App.js
+++ src/App.js
@@ -8,35 +8,47 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
-  const [failApiCount, setFailApiCount] = useState(0);
-  let timeoutId;
+  // const [failApiCount, setFailApiCount] = useState(0);
+  // let timeoutId;
 
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-http-997d5-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
-        setFailApiCount((pre) => {
-          console.log(pre);
-          if (pre === -1) return -1;
-          else return pre + 1;
-        });
+        // setFailApiCount((pre) => {
+        //   console.log(pre);
+        //   if (pre === -1) return -1;
+        //   else return pre + 1;
+        // });
         throw new Error("Something went wrong ......Retrying");
       }
 
       const data = await response.json();
+      
+      const loadedMovies = [];
 
-      const transformedMovies = data.results.map((moviesData) => {
-        return {
-          timeoutId: moviesData.episode_id,
-          title: moviesData.title,
-          openingText: moviesData.opening_crawl,
-          releaseDate: moviesData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate 
+        });
+      }
+      console.log(loadedMovies);
+
+      // const transformedMovies = data.results.map((moviesData) => {
+      //   return {
+      //     timeoutId: moviesData.episode_id,
+      //     title: moviesData.title,
+      //     openingText: moviesData.opening_crawl,
+      //     releaseDate: moviesData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -48,23 +60,49 @@ function App() {
   }, [fetchMoviesHandler]);
 
 
-  useEffect(() => {
-    // if (timeoutId) clearTimeout(timeoutId);
-    if (failApiCount > 0) {
-      timeoutId = setTimeout(() => {
-        fetchMoviesHandler();
-      }, 5000);
-    }
-  }, [failApiCount]);
+  // useEffect(() => {
+  //   // if (timeoutId) clearTimeout(timeoutId);
+  //   if (failApiCount > 0) {
+  //     timeoutId = setTimeout(() => {
+  //       fetchMoviesHandler();
+  //     }, 5000);
+  //   }
+  // }, [failApiCount]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch("https://react-http-997d5-default-rtdb.firebaseio.com/movies.json", {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      // do something
+    } else {
+      throw new Error("Something went wrong ......Retrying");
+    }
+    const data = await response.json();
+    console.log(data);
   }
+
+  async function deleteMovieHandler(id) {
+    console.log(id, "id");
+    // const response = await fetch(`https://react-http-997d5-default-rtdb.firebaseio.com/movies.json/${id}`, {
+    //   method: 'DELETE',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   }
+    // });
+    // const data = await response.json();
+    // console.log(data);
+  }  
 
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
+    content = <MoviesList movies={movies} delete={deleteMovieHandler} />;
   }
 
   if (error) {
@@ -82,13 +120,11 @@ function App() {
       </section>
       <section>
         <button
-          onClick={() => {
-            fetchMoviesHandler();
-          }}
+          onClick={fetchMoviesHandler}
         >
           Fetch Movies
         </button>
-        {failApiCount !== 0 && (
+        {/* {failApiCount !== 0 && (
           <button
             onClick={() => {
               setFailApiCount(-1);
@@ -97,7 +133,7 @@ function App() {
           >
             Cancel
           </button>
-        )}
+        )} */}
       </section>
       <section>{content}</section>
     </React.Fragment>
